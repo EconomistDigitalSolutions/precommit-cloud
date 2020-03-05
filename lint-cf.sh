9c19e44122999c8989c27a9137e72d05afda87c7
Lewis Turner <lewtur7@gmail.com>
2020-03-05 10:46:06 +0000

    do not error on cfn-lint warnings

--- lint-cf.sh
+++ lint-cf.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
+cfn-lint template.yml
+EXIT_CODE=$?
 
-cfn-lint template.yml && echo $?
+# Exit code 4 is warnings, which we don't want to error. See https://github.com/aws-cloudformation/cfn-python-lint/issues/235#issuecomment-406951041
+if [ $EXIT_CODE == 4 ]; then
+  EXIT_CODE=0
+fi
+
+exit $EXIT_CODE
